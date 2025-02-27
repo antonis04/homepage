fac157866f1a7321bc8abe93de4f62cdf7045367
Antek <antoni.samol@gmail.com>
2025-02-27 14:09:35 +0100

    added more funkctions

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,30 @@
 {
-    const welcome = ()  => {
+    const welcome = () => {
         console.log("Cześć deweloperzy! ten kod jest juz w repozytorium git");
     }
     welcome();
     
-    // Pobranie przycisku i kontenera na zdjęcie
     const przycisk = document.querySelector(".button");
     const zdjecieContainer = document.querySelector(".section__image-container");
-    
-    przycisk.addEventListener("click", () => {
+
+    const createImage = () => {
+        let noweZdjecie = document.createElement("img");
+        noweZdjecie.src = "https://i.ibb.co/wghrb1R/obraz.png";
+        noweZdjecie.alt = "Audi RS8";
+        noweZdjecie.classList.add("section__image");
+        return noweZdjecie;
+    };
+
+    const toggleImage = () => {
         let zdjecie = zdjecieContainer.querySelector(".section__image");
-    
         if (zdjecie) {
-            // Jeśli zdjęcie istnieje, usuń je
             zdjecie.remove();
             przycisk.textContent = "Pokaż audi";
         } else {
-            // Jeśli zdjęcia nie ma, dodaj je ponownie
-            let noweZdjecie = document.createElement("img");
-            noweZdjecie.src = "https://i.ibb.co/wghrb1R/obraz.png";
-            noweZdjecie.alt = "Audi RS8";
-            noweZdjecie.classList.add("section__image"); // Dodanie klasy BEM
-            zdjecieContainer.appendChild(noweZdjecie);
+            zdjecieContainer.appendChild(createImage());
             przycisk.textContent = "Ukryj audi";
         }
-    });
+    };
+
+    przycisk.addEventListener("click", toggleImage);
 }
