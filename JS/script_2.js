db19c1914d60361584108007100924c8583e09a1
Antek <antoni.samol@gmail.com>
2025-03-13 23:26:58 +0100

    object lesson 2 finished

---
+++ JS/script_2.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "John",
+    surname: "Doe",
+    age: 30,
+    gender: "male",
+
+};
+
+const { name, surname, age, gender } = person;
+
+console.log(name, surname, age, gender)
